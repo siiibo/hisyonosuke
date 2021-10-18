64d20e268f78d77eb82a56f084fadfd035652667
hi-se <hise.pr@gmail.com>
2021-10-18 14:16:36 +0900

    refactor: move init to app.ts

--- hisyonosuke/src/app.ts
+++ hisyonosuke/src/app.ts
@@ -92,5 +92,21 @@ export const getTypeAndCallbackId = (e: GoogleAppsScript.Events.DoPost): { type:
   }
 }
 
+const init = () => {
+  initProperties();
+}
+
+const initProperties = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('CONFIG');
+  const rows = sheet.getDataRange().getValues();
+  let properties = {};
+  for (let row of rows.slice(1)) properties[row[0]] = row[1];
+
+  const scriptProperties = PropertiesService.getScriptProperties();
+  scriptProperties.deleteAllProperties();
+  scriptProperties.setProperties(properties);
+}
+
 declare const global: any;
 global.doPost = doPost;
+global.init = init;
