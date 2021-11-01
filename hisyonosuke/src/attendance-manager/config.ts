8acb660711725e857ccdede2574783084ce83850
hi-se <hise.pr@gmail.com>
2021-11-01 15:50:12 +0900

    refactor: change props origin from spreadsheet

--- hisyonosuke/src/attendance-manager/config.ts
+++ hisyonosuke/src/attendance-manager/config.ts
@@ -9,14 +9,25 @@ interface Config {
 }
 
 export function getConfig(): Config {
-  // TODO: ScriptPropertyを挟む (パフォーマンスに影響がなければスプレッドシートのままで良いが)
+  const props = PropertiesService.getScriptProperties().getProperties();
+  return {
+    FREEE_CLIENT_ID: props['FREEE_CLIENT_ID'],
+    FREEE_CLIENT_SECRET: props['FREEE_CLIENT_SECRET'],
+    SLACK_TOKEN: props['SLACK_TOKEN'],
+    ATTENDANCE_CHANNEL_ID: props['ATTENDANCE_CHANNEL_ID'],
+    TEST_CHANNEL_ID: props['TEST_CHANNEL_ID']
+  }
+}
+
+export function initConfig(): void {
   const sheet = SpreadsheetApp.openById(CONFIG_SPREADSHEET_ID).getSheets()[0];
   const values = sheet.getRange(2, 2, sheet.getLastRow() - 1, 1).getValues().flat();
-  return {
+  const configs = {
     FREEE_CLIENT_ID: values[0],
     FREEE_CLIENT_SECRET: values[1],
     SLACK_TOKEN: values[2],
     ATTENDANCE_CHANNEL_ID: values[3],
     TEST_CHANNEL_ID: values[4]
   }
+  PropertiesService.getScriptProperties().setProperties(configs);
 }
