6f0e666fba0bc649d9434b11a6dd1d48e58b8c2b
hi-se <hise.pr@gmail.com>
2022-03-07 09:28:01 +0900

    fix: 休日判定処理を削除

--- hisyonosuke/src/attendance-manager/utilities.ts
+++ hisyonosuke/src/attendance-manager/utilities.ts
@@ -8,18 +8,3 @@ export function buildUrl(url: string, params: Object) {
 export function getUnixTimeStampString(date: Date): string {
   return Math.floor(date.getTime() / 1000).toFixed();
 }
-
-export function isWorkDay(targetDate: Date) {
-  return !isWeekend(targetDate) && !isHoliday(targetDate)
-}
-
-function isWeekend(targetDate: Date) {
-  return targetDate.getDay() === 0 || targetDate.getDay() === 6;
-}
-
-function isHoliday(targetDate: Date): boolean {
-  const calendarId = "ja.japanese#holiday@group.v.calendar.google.com";
-  const calendar = CalendarApp.getCalendarById(calendarId);
-  const todayEvents = calendar.getEventsForDay(targetDate);
-  return todayEvents.length > 0
-}
