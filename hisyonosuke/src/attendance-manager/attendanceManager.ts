d039c7f39cd595f43282a2b05eaee9a99bb98fce
hi-se <hise.pr@gmail.com>
2022-03-18 10:49:20 +0900

    refactor: reactionをグローバル変数に

--- hisyonosuke/src/attendance-manager/attendanceManager.ts
+++ hisyonosuke/src/attendance-manager/attendanceManager.ts
@@ -36,6 +36,12 @@ const COMMAND_TYPE = {
   CLOCK_OUT: [':taikin:', ':sagyoushuuryou:', ':saishuutaikin:', ':kinmushuuryou:']
 } as const;
 
+const REACTION = {
+  DONE_FOR_TIME_RECORD: 'dakoku_ok',
+  DONE_FOR_REMOTE_MEMO: 'memo_remote_ok',
+  DONE_FOR_LOCATION_SWITCH: 'switch_location_ok',
+  ERROR: 'dakoku_memo_error',
+}
 
 export const initAttendanceManager = () => {
   initConfig();
