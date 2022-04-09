cde88fed5a154c41da5ad7e265d67af262831248
hi-se <hise.pr@gmail.com>
2022-04-09 11:25:46 +0900

    refactor: 不要なコードの削除

--- hisyonosuke/src/attendance-manager/attendanceManager.ts
+++ hisyonosuke/src/attendance-manager/attendanceManager.ts
@@ -1,5 +1,3 @@
-import { GenericMessageEvent, SlackEvent } from '@slack/bolt';
-import { StringIndexed } from '@slack/bolt/dist/types/helpers';
 import { GasWebClient as SlackClient } from '@hi-se/web-api';
 import { format, setHours, setMinutes, setSeconds, subDays } from 'date-fns';
 
@@ -7,15 +5,6 @@ import { getCompanyEmployees, getWorkRecord, setTimeClocks, updateWorkRecord, Wo
 import { getUnixTimeStampString } from './utilities';
 import { getConfig, initConfig } from './config';
 import { Message } from '@hi-se/web-api/src/response/ConversationsHistoryResponse';
-
-enum IncomingEventType {
-  Event,
-  Action,
-  Command,
-  Options,
-  ViewAction,
-  Shortcut,
-}
 
 interface UserWorkStatus {
   workStatus: '勤務中（出社）' | '勤務中（リモート）' | '退勤済み' // 未出勤は現状利用していない
@@ -60,32 +49,12 @@ export const initAttendanceManager = () => {
     .create();
 }
 
-/**
- * doPost移行用
- * DoPost Eventのparse処理をdoPost内で行うように変更し、attendanceManagementに置き換える予定
- */
-export const attendanceManagerProxy = (e: GoogleAppsScript.Events.DoPost) => {
-  const payload = getPayload(e);
-  const incomingEventType = getIncomingEventType(payload);
 
-  attendanceManager(payload, incomingEventType);
-}
-
-/**
- * attendanceManagementのentry point
- */
-const attendanceManager = (payload: StringIndexed, incomingEventType: IncomingEventType) => {
-  switch (incomingEventType) {
-    case IncomingEventType.Event:
-      handleSlackEvent(payload.event as SlackEvent);
-      break;
-  }
-}
 
 export const periodicallyCheckForAttendanceManager = () => {
   const client = getSlackClient();
 
-  const { TEST_CHANNEL_ID, ATTENDANCE_CHANNEL_ID, PART_TIMER_CHANNEL_ID } = getConfig();
+  const { ATTENDANCE_CHANNEL_ID, PART_TIMER_CHANNEL_ID } = getConfig();
 
   // チャンネルごとに関数自体を分けて別プロセス（別のタイムトリガー）で動かすように変更する可能性あり
   checkAttendance(client, ATTENDANCE_CHANNEL_ID);
@@ -100,294 +69,6 @@ export const periodicallyCheckForAttendanceManager = () => {
   いずれPropServiceなどを使って状態管理するほうが良いかもしれない。
  */
 const checkAttendance = (client: SlackClient, channelId: string) => {
-  const { FREEE_COMPANY_ID } = getConfig();
-  const hisyonosukeUserId = 'U01AY3RHR42'; // ボットはbot_idとuser_idの2つのidを持ち、リアクションにはuser_idが使われる
-  const doneReactionForTimeRecord = 'dakoku_ok';
-  const doneReactionForRemoteMemo = 'memo_remote_ok';
-  const doneReactionForLocationSwitching = 'switch_location_ok';
-  const errorReaction = 'dakoku_memo_error';
-
-  const dateStartHour = 4;
-
-  const now = new Date();
-  let oldest = new Date();
-  oldest = setHours(oldest, dateStartHour);
-  oldest = setMinutes(oldest, 0);
-  oldest = setSeconds(oldest, 0);
-  if (now.getHours() <= dateStartHour) {
-    oldest = subDays(oldest, 1);
-  }
-
-  const messages = client.conversations.history({
-    channel: channelId,
-    oldest: getUnixTimeStampString(oldest),
-    inclusive: true
-  }).messages;
-
-  if (!messages?.length) {
-    return;
-  }
-
-  const messagesWithoutError = messages.filter(message => {
-    return !message.reactions?.some(reaction => {
-      return (
-        errorReaction === reaction.name &&
-        reaction.users.includes(hisyonosukeUserId)
-      );
-    })
-  });
-
-
-  const unprocessedClockIn = messagesWithoutError.filter(message => {
-    return message.text.match(/^\s*(:shukkin:|:shussha:|:sagyoukaishi:|:kinmukaishi:|:remoteshukkin:)\s*$/) &&
-      !message.reactions?.some(reaction => {
-        return (
-          [doneReactionForTimeRecord, doneReactionForLocationSwitching].includes(reaction.name) &&
-          reaction.users.includes(hisyonosukeUserId)
-        );
-      })
-  });
-
-  const unprocessedClockOut = messagesWithoutError.filter(message => {
-    return message.text.match(/^\s*(:taikin:|:sagyoushuuryou:|:saishuutaikin:|:kinmushuuryou:)\s*$/) &&
-      !message.reactions?.filter(reaction => {
-        return (
-          [doneReactionForTimeRecord].includes(reaction.name) &&
-          reaction.users.includes(hisyonosukeUserId)
-        );
-      }).length
-  });
-
-  const unprocessedRemote = messagesWithoutError.filter(message => {
-    return message.text.match(/^\s*(:remote:|:remoteshukkin:)\s*$/) &&
-      !message.reactions?.filter(reaction => {
-        return (
-          [doneReactionForRemoteMemo, doneReactionForLocationSwitching].includes(reaction.name) &&
-          reaction.users.includes(hisyonosukeUserId)
-        );
-      }).length
-  });
-
-  unprocessedClockIn.forEach(clockInMessage => {
-    const clockInDate = new Date(parseInt(clockInMessage.ts) * 1000);
-    const clockInBaseDate = new Date(clockInDate.getTime());
-
-    const clockInParams = {
-      company_id: FREEE_COMPANY_ID,
-      type: 'clock_in' as const,
-      base_date: format(clockInBaseDate, 'yyyy-MM-dd'),
-      datetime: format(clockInDate, 'yyyy-MM-dd HH:mm:ss')
-    };
-
-    let employeeId: number;
-
-    try {
-      employeeId = getFreeeEmployeeIdFromSlackUserId(client, clockInMessage.user, FREEE_COMPANY_ID);
-    } catch (e) {
-      console.error(e.stack);
-      console.error(`user:${employeeId}, type:${clockInParams.type}, base_date:${clockInParams.base_date}, datetime:${clockInParams.datetime}`);
-      const errorFeedBackMessage = e.toString();;
-      client.chat.postMessage({
-        channel: channelId,
-        text: errorFeedBackMessage,
-        thread_ts: clockInMessage.ts
-      });
-      client.reactions.add({
-        channel: channelId,
-        name: errorReaction,
-        timestamp: clockInMessage.ts
-      });
-
-      throw new Error(e); // FIXME: 後続の処理を走らせないためにおいているが、他の方法がありそう
-    }
-
-    const matchedUnprocessedRemote = unprocessedRemote.filter(remoteMessage => {
-      return remoteMessage.user === clockInMessage.user;
-    });
-
-    // リモート出勤 → 出社のパターン
-    if (matchedUnprocessedRemote.length === 1 && clockInMessage.text.match(/^\s*:shussha:\s*$/)) {
-      const remoteMessage = matchedUnprocessedRemote[0];
-      try {
-        client.reactions.add({
-          channel: channelId,
-          name: doneReactionForLocationSwitching,
-          timestamp: remoteMessage.ts
-        });
-        client.reactions.add({
-          channel: channelId,
-          name: doneReactionForLocationSwitching,
-          timestamp: clockInMessage.ts
-        });
-      } catch (e) {
-        console.error(e.stack);
-        console.error(`user:${employeeId}, type:${clockInParams.type}, base_date:${clockInParams.base_date}, datetime:${clockInParams.datetime}`);
-
-        const errorFeedBackMessage = e.toString(); //TODO: エラー内容の知見が溜まったら条件分岐を行う
-        client.chat.postMessage({
-          channel: channelId,
-          text: errorFeedBackMessage,
-          thread_ts: clockInMessage.ts
-        });
-        client.reactions.add({
-          channel: channelId,
-          name: errorReaction,
-          timestamp: clockInMessage.ts
-        });
-      }
-
-      return;
-    }
-
-    try {
-      setTimeClocks(employeeId, clockInParams);
-      client.reactions.add({
-        channel: channelId,
-        name: doneReactionForTimeRecord,
-        timestamp: clockInMessage.ts
-      });
-      console.info(`user:${employeeId}, type:${clockInParams.type}, base_date:${clockInParams.base_date}, datetime:${clockInParams.datetime}`);
-    } catch (e) {
-      console.error(e.stack);
-      console.error(`user:${employeeId}, type:${clockInParams.type}, base_date:${clockInParams.base_date}, datetime:${clockInParams.datetime}`);
-
-      let errorFeedBackMessage = '';
-      if (e.message.includes("打刻の種類が正しくありません。")) {
-        errorFeedBackMessage = '既に打刻済みです';
-      } else {
-        errorFeedBackMessage = e.toString();
-      }
-
-      client.chat.postMessage({
-        channel: channelId,
-        text: errorFeedBackMessage,
-        thread_ts: clockInMessage.ts
-      });
-      client.reactions.add({
-        channel: channelId,
-        name: errorReaction,
-        timestamp: clockInMessage.ts
-      });
-    }
-  });
-
-  unprocessedClockOut.forEach(clockOutMessage => {
-    const clockOutDate = new Date(parseInt(clockOutMessage.ts) * 1000);
-    const clockOutBaseDate = clockOutDate.getHours() > dateStartHour
-      ? new Date(clockOutDate.getTime())
-      : subDays(clockOutDate, 1);
-
-    const clockOutParams = {
-      company_id: FREEE_COMPANY_ID,
-      type: 'clock_out' as const,
-      base_date: format(clockOutBaseDate, 'yyyy-MM-dd'),
-      datetime: format(clockOutDate, 'yyyy-MM-dd HH:mm:ss')
-    };
-
-    const matchedUnprocessedRemote = unprocessedRemote.filter(remoteMessage => {
-      return remoteMessage.user === clockOutMessage.user;
-    });
-
-    let employeeId: number;
-    try {
-      employeeId = getFreeeEmployeeIdFromSlackUserId(client, clockOutMessage.user, FREEE_COMPANY_ID);
-    } catch (e) {
-      console.error(e.stack);
-      console.error(`user:${employeeId}, type:${clockOutParams.type}, base_date:${clockOutParams.base_date}, datetime:${clockOutParams.datetime}`);
-      const errorFeedBackMessage = e.toString();;
-      client.chat.postMessage({
-        channel: channelId,
-        text: errorFeedBackMessage,
-        thread_ts: clockOutMessage.ts
-      });
-      client.reactions.add({
-        channel: channelId,
-        name: errorReaction,
-        timestamp: clockOutMessage.ts
-      });
-
-      throw new Error(e); // FIXME: 後続の処理を走らせないためにおいているが、他の方法がありそう
-    }
-
-    try {
-      setTimeClocks(employeeId, clockOutParams);
-      client.reactions.add({
-        channel: channelId,
-        name: doneReactionForTimeRecord,
-        timestamp: clockOutMessage.ts
-      });
-      console.info(`user:${employeeId}, type:${clockOutParams.type}, base_date:${clockOutParams.base_date}, datetime:${clockOutParams.datetime}`);
-    } catch (e) {
-      // FIXME: 例外発生時の処理をちゃんと考える (出勤されていない場合など)
-      // NOTE: 退勤は打刻の重複が許容されているので出勤のエラー対応とは異なる
-      console.error(e.stack);
-      console.error(`user:${employeeId}, type:${clockOutParams.type}, base_date:${clockOutParams.base_date}, datetime:${clockOutParams.datetime}`);
-
-      let errorFeedBackMessage = '';
-      if (e.message.includes("打刻の種類が正しくありません。")) {
-        errorFeedBackMessage = '出勤打刻が完了していないか、退勤の上書きができない値です.';
-      } else {
-        errorFeedBackMessage = e.toString();
-      }
-
-      client.chat.postMessage({
-        channel: channelId,
-        text: errorFeedBackMessage,
-        thread_ts: clockOutMessage.ts
-      });
-      client.reactions.add({
-        channel: channelId,
-        name: errorReaction,
-        timestamp: clockOutMessage.ts
-      });
-    }
-    if (matchedUnprocessedRemote.length === 1) {
-      const remoteMessage = matchedUnprocessedRemote[0];
-      const targetDate = format(clockOutDate, 'yyyy-MM-dd');
-      const workRecord = getWorkRecord(employeeId, targetDate, FREEE_COMPANY_ID);
-      const remoteParams: WorkRecordControllerRequestBody = {
-        company_id: FREEE_COMPANY_ID,
-        clock_in_at: format(new Date(workRecord.clock_in_at), 'yyyy-MM-dd HH:mm:ss'),
-        clock_out_at: format(new Date(workRecord.clock_out_at), 'yyyy-MM-dd HH:mm:ss'),
-        note: workRecord.note ? `${workRecord.note} リモート` : 'リモート',
-        break_records: workRecord.break_records.map(record => {
-          return {
-            clock_in_at: format(new Date(record.clock_in_at), 'yyyy-MM-dd HH:mm:ss'),
-            clock_out_at: format(new Date(record.clock_out_at), 'yyyy-MM-dd HH:mm:ss')
-          }
-        })
-      }
-
-      try {
-        updateWorkRecord(employeeId, targetDate, remoteParams);
-        client.reactions.add({
-          channel: channelId,
-          name: doneReactionForRemoteMemo,
-          timestamp: remoteMessage.ts
-        });
-        console.info(`user:${employeeId}, type:remote, clock_in_at:${remoteParams.clock_in_at}, clock_out_at:${remoteParams.clock_out_at}, note:${remoteParams.note}`);
-
-      } catch (e) {
-        console.error(e.stack);
-        console.error(`user:${employeeId}, type:remote, clock_in_at:${remoteParams.clock_in_at}, clock_out_at:${remoteParams.clock_out_at}, note:${remoteParams.note}`);
-
-        const errorFeedBackMessage = e.toString(); //TODO: エラー内容の知見が溜まったら条件分岐を行う
-        client.chat.postMessage({
-          channel: channelId,
-          text: errorFeedBackMessage,
-          thread_ts: remoteMessage.ts
-        });
-        client.reactions.add({
-          channel: channelId,
-          name: errorReaction,
-          timestamp: remoteMessage.ts
-        });
-      }
-    }
-  });
-}
-
-const _checkAttendance = (client: SlackClient, channelId: string) => {
   const hisyonosukeUserId = 'U01AY3RHR42'; // ボットはbot_idとuser_idの2つのidを持ち、リアクションにはuser_idが使われる
 
   const messages = getDailyMessages(client, channelId);
@@ -414,7 +95,6 @@ const _checkAttendance = (client: SlackClient, channelId: string) => {
   });
 
   const { FREEE_COMPANY_ID } = getConfig();
-
   actionsToProcess.forEach((action) => {
     execAction(client, channelId, FREEE_COMPANY_ID, action);
   });
@@ -758,64 +438,6 @@ const getCommandType = (message: Message): CommandType | undefined => {
   }
 }
 
-
-// タイムトリガー形式にしたので削除予定
-const handleSlackEvent = (event: SlackEvent) => {
-  console.log(event);
-  const client = getSlackClient();
-  switch (event.type) {
-    case 'message':
-      const message = getMessageListener(client, event);
-      // TODO: ここではスタンプ押した際に投稿者へのフィードバックを行う。
-      message(/:shukkin:|:shussha:|:sagyoukaishi:|:kinmukaishi:|:remoteshukkin:/, ({ client, event }) => {
-        client.chat.postMessage({
-          text: '出勤テスト（ephemeralに変更予定）',
-          channel: event.channel,
-        });
-      })
-
-      message(/:taikin:|:sagyoushuuryou:|:saishuutaikin:|:kinmushuuryou:/, ({ client, event }) => {
-        client.chat.postMessage({
-          text: '退勤テスト（ephemeralに変更予定）',
-          channel: event.channel,
-        });
-      })
-
-      message(/:remote:|:remoteshukkin:/, ({ client, event }) => {
-        console.log(event);
-      })
-      break;
-  }
-}
-
-
-
-const getMessageListener = (client: SlackClient, event: SlackEvent) => {
-  const isOriginalCommand = (target: string, command: string | RegExp) => {
-    if (typeof command === 'string') {
-      const commandRegExp = new RegExp(command);
-      return target.match(commandRegExp);
-    } else if (command instanceof RegExp) {
-      // 週次之介のように前後の文字列でさらに制限をかける場合は一旦stringにする必要がある
-      // その場合、RegExp.prototype.toString()はスラッシュとflagを含めて返すため、取り除く処理が必要
-      return target.match(command)
-    } else {
-      throw new Error('TypeError');
-    }
-  };
-
-  if (event.type === 'message') {
-    if (!event.subtype) {
-      // とりあえずsubtypeを持たない pure message だけ対応
-      return (command: string | RegExp, callback: ({ client, event }: { client: SlackClient, event: GenericMessageEvent }) => void) => {
-        if (isOriginalCommand((event as GenericMessageEvent).text, command)) {
-          callback({ client, event: event as GenericMessageEvent });
-        }
-      }
-    }
-  }
-}
-
 const getFreeeEmployeeIdFromSlackUserId = (client: SlackClient, slackUserId: string, companyId: number): number => {
   // TODO: PropertiesService等を挟むようにする（毎回APIを投げない）
   const email = client.users.info({
@@ -841,36 +463,3 @@ const getSlackClient = () => {
   const token = PropertiesService.getScriptProperties().getProperty('SLACK_TOKEN');
   return new SlackClient(token);
 }
-
-const getPayload = (e: GoogleAppsScript.Events.DoPost): StringIndexed => {
-  if (e.postData.type === "application/json") {
-    return JSON.parse(e.postData.contents);
-  } else if (e.postData.type === "application/x-www-form-urlencoded") {
-    return e.parameter
-  } else {
-    throw new Error('Invalid incoming payload.');
-  }
-}
-
-const getIncomingEventType = (body: StringIndexed): IncomingEventType => {
-  if (body.event) {
-    return IncomingEventType.Event;
-  }
-  if (body.command) {
-    return IncomingEventType.Command;
-  }
-  if (body.name || body.type === 'block_suggestion') {
-    return IncomingEventType.Options;
-  }
-  if (body.actions || body.type === ('dialog_submission' || 'workflow_step_edit')) {
-    return IncomingEventType.Action;
-  }
-  if (body.type === ('shortcut' || 'message_action')) {
-    return IncomingEventType.Shortcut;
-  }
-  if (body.type === 'view_submission' || body.type === 'view_closed') {
-    return IncomingEventType.ViewAction;
-  }
-  return undefined;
-}
-
