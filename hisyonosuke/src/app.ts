2124faa219d08f5e74ec6612eb70d6149ca39d77
hi-se <40375981+hi-se@users.noreply.github.com>
2022-06-01 16:24:00 +0900

    [feature] 週次之介から一部機能を移行 (#12)
    * feat: migrate from shujinosuke
    
    * feat: 絵文字・チャンネルの変更検知

--- hisyonosuke/src/app.ts
+++ hisyonosuke/src/app.ts
@@ -1,10 +1,15 @@
-import { SlackAction, SlackEvent, SlackShortcut, SlackViewAction } from '@slack/bolt';
+import { ChannelCreatedEvent, EmojiChangedEvent, SlackAction, SlackEvent, SlackShortcut, SlackViewAction } from '@slack/bolt';
+import { GasWebClient as SlackClient } from '@hi-se/web-api';
 import { birthdayRegistrator } from './birthday-registrator/birthday-registrator';
 import { workflowCustomStep } from './workflow-customstep/workflow-customstep';
 import { notificator } from './notificator';
 import { periodicallyCheckForAttendanceManager, initAttendanceManager } from './attendance-manager/attendanceManager'
 
 const PROPS_SPREADSHEET_ID = '1Kuq2VaGe96zn0G3LG7OxapLZ0aQvYMqOW9IlorwbJoU';
+
+// Shujinosukeから移行 // TODO: いつか全体を整えたらコメント消す
+const EMOJI_EVENT_POST_CHANNEL = "C011BG29K71" // #雑談
+const CHANNEL_EVENT_POST_CHANNEL = "C011BG29K71"; // #雑談
 
 const doPost = (e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.TextOutput => {
   console.info({
@@ -16,10 +21,56 @@ const doPost = (e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.Tex
     return ContentService.createTextOutput(JSON.parse(e.postData.contents)['challenge']);
   }
 
+  // Shujinosukeから移行 // TODO: いつか全体を整えたらコメント消す
+  if (isEvent(e)) {
+    const event = JSON.parse(e.postData.contents)['event'] as SlackEvent;
+    const client = getSlackClient();
+    if (event.type === 'app_mention') {
+      if (isOriginalCommand(event.text, 'アルバイトシフト')) {
+        const calendarId = 'c_1889m1jd2rticjeig08cshi84mnrs4gaedkmiqb2dsn66rrd@resource.calendar.google.com';
+        const calendar = CalendarApp.getCalendarById(calendarId);
+        const targetDate = new Date();
+        const dailyShifts = calendar.getEventsForDay(targetDate);
+
+        if (!dailyShifts.length) {
+          client.chat.postMessage({
+            channel: event.channel,
+            text: '今日の予定はありません'
+          });
+          return;
+        }
+
+        const notificationString = dailyShifts.map(dailyShift => {
+          const title = dailyShift.getTitle()
+          const startTime = Utilities.formatDate(dailyShift.getStartTime(), 'Asia/Tokyo', 'HH:mm');
+          const endTime = Utilities.formatDate(dailyShift.getEndTime(), 'Asia/Tokyo', 'HH:mm');
+          return `${title}  ${startTime} 〜 ${endTime}`
+        }).join('\n');
+
+        client.chat.postMessage({
+          channel: event.channel,
+          text: notificationString
+        });
+      }
+    }
+    if (event.type === 'emoji_changed') {
+      handleEmojiChange(client, event as EmojiChangedEvent);
+    }
+    if (event.type === 'channel_created') {
+      handleChannelCreated(client, event as ChannelCreatedEvent);
+    }
+  }
+
   const response = birthdayRegistrator(e); // FIXME: レスポンスの書き換えが生じないようにとりあえずconstで定義してある
   workflowCustomStep(e);
 
   return ContentService.createTextOutput(response).setMimeType(ContentService.MimeType.JSON);
+}
+
+// attendanceManager.ts から移行 // TODO: いつか全体を整えたらコメント消す
+const getSlackClient = () => {
+  const token = PropertiesService.getScriptProperties().getProperty('SLACK_TOKEN');
+  return new SlackClient(token);
 }
 
 const isJson = (e: GoogleAppsScript.Events.DoPost): boolean => {
@@ -103,6 +154,39 @@ export const getTypeAndCallbackId = (e: GoogleAppsScript.Events.DoPost): { type:
         return { type: payload.type, callback_id: undefined };
     }
   }
+}
+
+
+// Shujinosukeから移行 // TODO: いつか全体を整えたらコメント消す
+const isOriginalCommand = (target: string, commandRegExpString: string) => {
+  const regExpString = {
+    slackMarkUp: "([*_~`>]|`{3,})*",
+    slackMention: "<@\\w+[\\w\\s\|]*>\\s+",
+    commandEnd: "($|[\\s.]+)", // SlackBotのリマインダーで英字コマンドを呼び出すと文末にピリオド(.)が追加される
+  }
+  const commandRegExp = new RegExp(
+    regExpString.slackMention +
+    regExpString.slackMarkUp +
+    commandRegExpString +
+    regExpString.slackMarkUp +
+    regExpString.commandEnd
+  );
+  return target.match(commandRegExp);
+}
+const handleEmojiChange = (client: SlackClient, event: EmojiChangedEvent) => {
+  if (event.subtype === 'add') {
+    client.chat.postMessage({
+      channel: EMOJI_EVENT_POST_CHANNEL,
+      text: `:${event.name}:  (\`:${event.name}:\`)が追加されました！`
+    });
+  }
+}
+
+const handleChannelCreated = (client: SlackClient, event: ChannelCreatedEvent) => {
+  client.chat.postMessage({
+    channel: CHANNEL_EVENT_POST_CHANNEL,
+    text: `<#${event.channel.id}>が追加されました！`
+  });
 }
 
 const init = () => {
