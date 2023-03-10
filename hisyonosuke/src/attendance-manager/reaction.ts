5df3994a72504cf98688bbf3ca262d954fa1602c
hi-se <40375981+hi-se@users.noreply.github.com>
2023-03-10 16:29:37 +0900

    refactor: reactionを別ファイルへ

---
+++ hisyonosuke/src/attendance-manager/reaction.ts
@@ -0,0 +1,6 @@
+export const REACTION = {
+  DONE_FOR_TIME_RECORD: 'dakoku_ok',
+  DONE_FOR_REMOTE_MEMO: 'memo_remote_ok',
+  DONE_FOR_LOCATION_SWITCH: 'switch_location_ok',
+  ERROR: 'dakoku_memo_error',
+}
