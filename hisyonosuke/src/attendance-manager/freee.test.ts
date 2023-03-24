fe135888c973e972cf09dbb0ed2f83396a68ef42
hi-se <40375981+hi-se@users.noreply.github.com>
2023-03-24 14:51:23 +0900

    test: getWorkTimeSumにから配列を渡したときのテスト追加

--- hisyonosuke/src/attendance-manager/freee.test.ts
+++ hisyonosuke/src/attendance-manager/freee.test.ts
@@ -1,6 +1,11 @@
 import { getTotalTimeFromTimeRanges } from "./freee";
+import { EmployeesWorkRecordTimeRangeSerializer } from "./freee.schema";
 
 describe("getWorkTimeSum", () => {
+  it("should return 0 if no time range", () => {
+    const timeRanges: EmployeesWorkRecordTimeRangeSerializer[] = [];
+    expect(getTotalTimeFromTimeRanges(timeRanges)).toBe(0);
+  });
   it("should return the sum of work time", () => {
     const timeRanges = [{ clock_in_at: "2023-01-01T09:00:00", clock_out_at: "2023-01-01T17:00:00" }];
     expect(getTotalTimeFromTimeRanges(timeRanges)).toBe(60 * 60 * 8 * 1000);
