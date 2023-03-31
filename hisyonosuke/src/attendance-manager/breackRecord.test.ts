29563ef9c3d28574d3d68ac8e37c595e93c7929a
hi-se <40375981+hi-se@users.noreply.github.com>
2023-03-31 13:47:10 +0900

    feat: 追加する休憩を計算する関数の作成

---
+++ hisyonosuke/src/attendance-manager/breackRecord.test.ts
@@ -0,0 +1,123 @@
+import { createAdditionalBreakTime, calculateBreakTimeMsToAdd } from "./breackRecord";
+
+describe("calculateBreakTimeToAdd", () => {
+  it("労働時間が6時間30分以上8時間未満、休憩時間が45分未満の場合、休憩時間を45分になるように差分を返す", () => {
+    const clock_in_at = "2020-01-01T09:00:00.000Z";
+    const clock_out_at = "2020-01-01T15:30:00.000Z";
+    const break_records = [{ clock_in_at: "2020-01-01T12:00:00.000Z", clock_out_at: "2020-01-01T12:30:00.000Z" }];
+    expect(calculateBreakTimeMsToAdd({ clock_in_at, clock_out_at, break_records })).toBe(900000);
+  });
+
+  it("労働時間が8時間以上、休憩時間が1時間未満の場合、休憩時間を1時間になるように差分を返す", () => {
+    const clock_in_at = "2020-01-01T09:00:00.000Z";
+    const clock_out_at = "2020-01-01T17:00:00.000Z";
+    const break_records = [{ clock_in_at: "2020-01-01T12:00:00.000Z", clock_out_at: "2020-01-01T12:30:00.000Z" }];
+    expect(calculateBreakTimeMsToAdd({ clock_in_at, clock_out_at, break_records })).toBe(1800000);
+  });
+
+  it("労働時間が6時間30分以上8時間未満、休憩時間が45分以上の場合、何もしない", () => {
+    const clock_in_at = "2020-01-01T09:00:00.000Z";
+    const clock_out_at = "2020-01-01T15:30:00.000Z";
+    const break_records = [{ clock_in_at: "2020-01-01T12:00:00.000Z", clock_out_at: "2020-01-01T13:00:00.000Z" }];
+    expect(calculateBreakTimeMsToAdd({ clock_in_at, clock_out_at, break_records })).toBe(0);
+  });
+
+  it("労働時間が8時間以上、休憩時間が1時間以上の場合、何もしない", () => {
+    const clock_in_at = "2020-01-01T09:00:00.000Z";
+    const clock_out_at = "2020-01-01T17:00:00.000Z";
+    const break_records = [{ clock_in_at: "2020-01-01T12:00:00.000Z", clock_out_at: "2020-01-01T13:00:00.000Z" }];
+    expect(calculateBreakTimeMsToAdd({ clock_in_at, clock_out_at, break_records })).toBe(0);
+  });
+});
+
+describe("休憩時間の追加テスト", () => {
+  const clockInBefore13 = "2023-03-30 09:00:00";
+  const clockInAfter13 = "2023-03-30 14:00:00";
+  const clockOutAfter13 = "2023-03-30 18:00:00";
+  const clockOutBefore13 = "2023-03-30 11:00:00";
+  const breakDurationMs = 60 * 60 * 1000; // 1時間
+
+  test("休憩時間が0, 出勤時間が13時より前, 退勤時間が13時より後の場合、13時〜休憩を追加する", () => {
+    const workRecord = {
+      clock_in_at: clockInBefore13,
+      clock_out_at: clockOutAfter13,
+      break_records: [],
+    };
+    const expectedBreakRecord = {
+      clock_in_at: "2023-03-30 13:00:00",
+      clock_out_at: "2023-03-30 14:00:00",
+    };
+    const result = createAdditionalBreakTime(workRecord, breakDurationMs);
+    expect(result).toEqual(expectedBreakRecord);
+  });
+
+  test("休憩時間が0, 出勤時間が13時より前, 退勤時間が13時より前の場合、出勤時間から1h後に休憩を追加する", () => {
+    const workRecord = {
+      clock_in_at: clockInBefore13,
+      clock_out_at: clockOutBefore13,
+      break_records: [],
+    };
+    const expectedBreakRecord = {
+      clock_in_at: "2023-03-30 10:00:00",
+      clock_out_at: "2023-03-30 11:00:00",
+    };
+    const result = createAdditionalBreakTime(workRecord, breakDurationMs);
+    expect(result).toEqual(expectedBreakRecord);
+  });
+
+  test("休憩時間が0, 出勤時間が13時より後, 退勤時間が13時より後の場合、出勤時間から1h後に休憩を追加する", () => {
+    const workRecord = {
+      clock_in_at: clockInAfter13,
+      clock_out_at: clockOutAfter13,
+      break_records: [],
+    };
+    const expectedBreakRecord = {
+      clock_in_at: "2023-03-30 15:00:00",
+      clock_out_at: "2023-03-30 16:00:00",
+    };
+    const result = createAdditionalBreakTime(workRecord, breakDurationMs);
+    expect(result).toEqual(expectedBreakRecord);
+  });
+
+  test("休憩時間がすでにある, 最後の休憩時間に休憩時間を追加しても退勤時間を超えない場合、最後の休憩時間の直後に休憩を追加する", () => {
+    const workRecord = {
+      clock_in_at: clockInBefore13,
+      clock_out_at: clockOutAfter13,
+      break_records: [
+        {
+          clock_in_at: "2023-03-30 10:00:00",
+          clock_out_at: "2023-03-30 11:00:00",
+        },
+      ],
+    };
+    const expectedBreakRecord = {
+      clock_in_at: "2023-03-30 11:00:00",
+      clock_out_at: "2023-03-30 12:00:00",
+    };
+    const result = createAdditionalBreakTime(workRecord, breakDurationMs);
+    expect(result).toEqual(expectedBreakRecord);
+  });
+
+  test("休憩時間がすでにある, 最後の休憩時間に休憩時間を追加すると退勤時間を超えてしまう場合、最初の休憩開始時間の直前に休憩を追加する", () => {
+    const workRecord = {
+      clock_in_at: clockInBefore13,
+      clock_out_at: clockOutAfter13,
+      break_records: [
+        {
+          clock_in_at: "2023-03-30 11:00:00",
+          clock_out_at: "2023-03-30 12:00:00",
+        },
+        {
+          clock_in_at: "2023-03-30 17:00:00",
+          clock_out_at: "2023-03-30 17:30:00",
+        },
+      ],
+    };
+    const expectedBreakRecord = {
+      clock_in_at: "2023-03-30 10:00:00",
+      clock_out_at: "2023-03-30 11:00:00",
+    };
+    const result = createAdditionalBreakTime(workRecord, breakDurationMs);
+    expect(result).toEqual(expectedBreakRecord);
+  });
+});
---
+++ hisyonosuke/src/attendance-manager/breackRecord.ts
@@ -0,0 +1,114 @@
+import {
+  addHours,
+  millisecondsToMinutes,
+  minutesToMilliseconds,
+  set,
+  addMilliseconds,
+  isBefore,
+  isAfter,
+  format,
+  subMilliseconds,
+} from "date-fns";
+import { match, P } from "ts-pattern";
+import { getTotalTimeFromTimeRanges } from "./freee";
+import { EmployeesWorkRecordTimeRangeSerializer } from "./freee.schema";
+import * as R from "remeda";
+
+type BreakRecordWithClockInAndOut = EmployeesWorkRecordTimeRangeSerializer & {
+  break_records: EmployeesWorkRecordTimeRangeSerializer[];
+};
+
+export function calculateBreakTimeMsToAdd({ clock_in_at, clock_out_at, break_records }: BreakRecordWithClockInAndOut) {
+  const totalWorkTimeMs = getTotalTimeFromTimeRanges([{ clock_in_at, clock_out_at }]);
+  const totalBreakTimeMs = getTotalTimeFromTimeRanges(break_records);
+
+  /**
+   * 追加が必要な休憩時間
+   *
+   * (totalWorkTime, totalBreakTime) が
+   * (6h30min以上8h未満, 45min未満)の場合 → totalBreakTimeが45minになるように差分を返す
+   * (8h以上, 1h未満)の場合 → totalBreakTimeが1hになるように差分を返す
+   * その他の場合は何もしない
+   */
+  const needToAddBreakTimeMin = match([millisecondsToMinutes(totalWorkTimeMs), millisecondsToMinutes(totalBreakTimeMs)])
+    .with(
+      P.when(
+        ([totalWorkTimeMin, totalBreakTimeMin]) =>
+          totalWorkTimeMin >= 6 * 60 + 30 && totalWorkTimeMin < 8 * 60 && totalBreakTimeMin < 45
+      ),
+      ([, totalBreakTimeMin]) => 45 - totalBreakTimeMin
+    )
+    .with(
+      P.when(([totalWorkTimeMin, totalBreakTimeMin]) => totalWorkTimeMin >= 8 * 60 && totalBreakTimeMin < 60),
+      ([, totalBreakTimeMin]) => 60 - totalBreakTimeMin
+    )
+    .otherwise(() => 0);
+  console.log([totalWorkTimeMs, totalBreakTimeMs]);
+  return minutesToMilliseconds(needToAddBreakTimeMin);
+}
+
+export const createNewTimeRecord = (startTime: Date, breakDuration: number): EmployeesWorkRecordTimeRangeSerializer => {
+  const clock_in_at = startTime;
+  const clock_out_at = addMilliseconds(clock_in_at, breakDuration);
+  return {
+    clock_in_at: format(clock_in_at, "yyyy-MM-dd HH:mm:ss"),
+    clock_out_at: format(clock_out_at, "yyyy-MM-dd HH:mm:ss"),
+  };
+};
+
+// 休憩時間を追加する関数
+export function createAdditionalBreakTime(
+  workRecord: BreakRecordWithClockInAndOut,
+  breakDurationMs: number
+): {
+  clock_in_at: string;
+  clock_out_at: string;
+} {
+  const [clock_in_at, clock_out_at, break_records] = [
+    new Date(workRecord.clock_in_at),
+    new Date(workRecord.clock_out_at),
+    workRecord.break_records
+      .map((breakRecord) => ({
+        clock_in_at: new Date(breakRecord.clock_in_at),
+        clock_out_at: new Date(breakRecord.clock_out_at),
+      }))
+      .sort((a, b) => a.clock_in_at.getTime() - b.clock_in_at.getTime()),
+  ];
+  const PM_1 = set(clock_in_at, { hours: 13, minutes: 0, seconds: 0, milliseconds: 0 });
+
+  if (break_records.length === 0) {
+    const newBreakRecord = match([clock_in_at, clock_out_at])
+      .with(
+        P.when(([clockInAt, clockOutAt]) => isBefore(clockOutAt, PM_1) || isAfter(clockInAt, PM_1)),
+        ([clockInAt]) => {
+          return createNewTimeRecord(addHours(clockInAt, 1), breakDurationMs);
+        }
+      )
+      .otherwise(() => createNewTimeRecord(PM_1, breakDurationMs));
+    return newBreakRecord;
+  } else {
+    const lastBreak = R.last(break_records);
+    const firstBreak = R.first(break_records);
+    if (!lastBreak || !firstBreak) throw new Error(`Cannot add break time. ${JSON.stringify(workRecord)}`);
+
+    const breakStart = match([firstBreak.clock_in_at, lastBreak.clock_out_at])
+      .with(
+        P.when(([, lastBreakClockOutAt]) =>
+          isBefore(addMilliseconds(lastBreakClockOutAt, breakDurationMs), clock_out_at)
+        ),
+        ([, lastBreakClockOutAt]) => lastBreakClockOutAt
+      )
+      .with(
+        P.when(([firstBreakClockInAt]) => isAfter(subMilliseconds(firstBreakClockInAt, breakDurationMs), clock_in_at)),
+        ([firstBreakClockInAt]) => subMilliseconds(firstBreakClockInAt, breakDurationMs)
+      )
+      .otherwise((firstLastBreak) => {
+        throw new Error(
+          `Cannot add break time.\nargs: ${JSON.stringify(firstLastBreak)}\n${JSON.stringify(workRecord)}`
+        );
+      });
+
+    const newBreakRecord = createNewTimeRecord(breakStart, breakDurationMs);
+    return newBreakRecord;
+  }
+}
