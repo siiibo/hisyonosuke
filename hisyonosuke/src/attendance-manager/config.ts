28aa6cbd4ece3378a212aa4c6aa6889b11ad3f60
hi-se <40375981+hi-se@users.noreply.github.com>
2023-04-03 15:44:54 +0900

    refactor: Configをzod形式に変更

--- hisyonosuke/src/attendance-manager/config.ts
+++ hisyonosuke/src/attendance-manager/config.ts
@@ -1,26 +1,22 @@
+import { z } from "zod";
+
 const CONFIG_SPREADSHEET_ID = "1fAxgi14OJx6f8RrnUMMoELF--mSZPMWgl3Zz_CUGYL8";
 
-interface Config {
-  FREEE_CLIENT_ID: string;
-  FREEE_CLIENT_SECRET: string;
-  FREEE_COMPANY_ID: number;
-  SLACK_TOKEN: string;
-  ATTENDANCE_CHANNEL_ID: string;
-  PART_TIMER_CHANNEL_ID: string;
-  TEST_CHANNEL_ID: string;
-}
+const ConfigSchema = z.object({
+  FREEE_CLIENT_ID: z.string(),
+  FREEE_CLIENT_SECRET: z.string(),
+  FREEE_COMPANY_ID: z.preprocess((v) => Number(v), z.number()),
+  SLACK_TOKEN: z.string(),
+  ATTENDANCE_CHANNEL_ID: z.string(),
+  PART_TIMER_CHANNEL_ID: z.string(),
+  TEST_CHANNEL_ID: z.string(),
+});
+
+export type Config = z.infer<typeof ConfigSchema>;
 
 export function getConfig(): Config {
   const props = PropertiesService.getScriptProperties().getProperties();
-  return {
-    FREEE_CLIENT_ID: props["FREEE_CLIENT_ID"],
-    FREEE_CLIENT_SECRET: props["FREEE_CLIENT_SECRET"],
-    FREEE_COMPANY_ID: Number(props["FREEE_COMPANY_ID"]),
-    SLACK_TOKEN: props["SLACK_TOKEN"],
-    ATTENDANCE_CHANNEL_ID: props["ATTENDANCE_CHANNEL_ID"],
-    PART_TIMER_CHANNEL_ID: props["PART_TIMER_CHANNEL_ID"],
-    TEST_CHANNEL_ID: props["TEST_CHANNEL_ID"],
-  };
+  return ConfigSchema.parse(props);
 }
 
 export function initConfig(): void {
