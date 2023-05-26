841974e946791b1354ac1034b75c5382f9ef3ff8
Luliko_hub <snow-child@keio.jp>
2023-05-26 04:02:46 +0000

    feat: GASエディタから実行する関数の変更

--- hisyonosuke/src/index.ts
+++ hisyonosuke/src/index.ts
@@ -1,16 +1,36 @@
-import { doPost, init } from "./app";
+// import { doPost, init } from "./app";
 import { notificator } from "./birthday-registrator/notificator";
 import { periodicallyCheckForAttendanceManager } from "./attendance-manager/attendanceManager";
 import { notifyPartTimerShift } from "./part-timer-shift/notify";
-
+import {
+  doPost,
+  // init,
+  onOpen,
+  callRegistration,
+  callShowEvents,
+  callModificationAndDeletion,
+  insertRegistrationSheet,
+  insertModificationAndDeletionSheet,
+  // onInstall,
+} from "./shift-changer/shift-changer";
 /**
  * @file GASエディタから実行できる関数を定義する
  */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const global: any;
+// global.doPost = doPost;
+// global.init = init;
+// global.notificator = notificator;
+// global.periodicallyCheckForAttendanceManager = periodicallyCheckForAttendanceManager;
+
 global.doPost = doPost;
-global.init = init;
-global.notificator = notificator;
-global.periodicallyCheckForAttendanceManager = periodicallyCheckForAttendanceManager;
+// global.init = init;
+// global.onInstall = onInstall;
+global.onOpen = onOpen;
+global.callRegistration = callRegistration;
+global.callShowEvents = callShowEvents;
+global.callModificationAndDeletion = callModificationAndDeletion;
+global.insertRegistrationSheet = insertRegistrationSheet;
+global.insertModificationAndDeletionSheet = insertModificationAndDeletionSheet;
 global.notifyPartTimerShift = notifyPartTimerShift;
