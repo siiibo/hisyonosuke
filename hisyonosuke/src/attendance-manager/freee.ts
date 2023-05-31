39da66d2bd743196e191bb13cf412cabbc316ad7
hi-se <40375981+hi-se@users.noreply.github.com>
2023-05-31 10:48:57 +0900

    Refactor(attendance-manager): Freee clientをクラス化 (#35)
    別アプリでスプレッドシートのonOpen関数を利用する際にScriptProperties取得権限がないというエラーが発生した。
    ([Trelloコメント](https://trello.com/c/9P3k440s/7-%E3%82%A2%E3%83%AB%E3%83%90%E3%82%A4%E3%83%88%E3%81%AE%E3%82%B7%E3%83%95%E3%83%88%E5%A4%89%E6%9B%B4%E7%94%A8%E3%83%84%E3%83%BC%E3%83%AB%E3%81%AE%E4%BD%9C%E6%88%90#comment-64769f68762228285567eb92)）
    明示的に呼び出すまで処理を実行しないように一旦クラス化した。
    
    Slack Appとしては開発していないのでそもそもプロジェクトを分ければよいが、今から移動するのは少し大変そう &
    方針も曖昧なのでこちら側で対応する。
    
    
    
    🔗  https://trello.com/c/9P3k440s

--- hisyonosuke/src/attendance-manager/freee.ts
+++ hisyonosuke/src/attendance-manager/freee.ts
@@ -8,58 +8,94 @@ import type {
 } from "./freee.schema";
 import { getService } from "./auth";
 import { buildUrl } from "./utilities";
-import { getConfig } from "./config";
 
-const fetch =
-  getConfig().ATTENDANCE_MANAGER_ENV === "production" ? createFetch(getService().getAccessToken()) : UrlFetchApp.fetch;
+export class Freee {
+  private fetch;
 
-function createFetch(accessToken: string) {
-  return <Schema>(
-    url: string,
-    options: {
-      method: "get" | "post" | "put" | "delete";
-      body?: unknown;
-      schema?: z.ZodType<Schema>;
-    }
-  ): Result<Schema, string> => {
-    const wrappedFetch = () => {
-      const response = UrlFetchApp.fetch(url, {
-        headers: {
-          Authorization: "Bearer " + accessToken,
-          "FREEE-VERSION": "2022-02-01",
+  constructor() {
+    this.fetch = this.createFetch(getService().getAccessToken());
+  }
+
+  private createFetch(accessToken: string) {
+    return <Schema>(
+      url: string,
+      options: {
+        method: "get" | "post" | "put" | "delete";
+        body?: unknown;
+        schema?: z.ZodType<Schema>;
+      }
+    ): Result<Schema, string> => {
+      const wrappedFetch = () => {
+        const response = UrlFetchApp.fetch(url, {
+          headers: {
+            Authorization: "Bearer " + accessToken,
+            "FREEE-VERSION": "2022-02-01",
+          },
+          method: options.method,
+          muteHttpExceptions: true,
+          ...(["post", "put"].includes(options.method) && {
+            payload: JSON.stringify(options.body),
+            contentType: "application/json",
+          }),
+        });
+
+        const responseCode = response.getResponseCode();
+        const responseBody = response.getContentText();
+
+        return match(responseCode)
+          .with(200, 201, () => {
+            if (!options.schema) return ok(JSON.parse(responseBody));
+            const parseResult = options.schema.safeParse(JSON.parse(responseBody));
+            return parseResult.success ? ok(parseResult.data) : err(parseResult.error.message);
+          })
+          .with(500, () => {
+            return err("Error: Internal Server Error");
+          })
+          .otherwise(() => err(responseBody));
+      };
+      return withRetry(
+        {
+          times: 3,
+          delay: 1000,
+          backoff: (count) => count * 1000,
+          shouldRetry: (error) => error === "Error: Internal Server Error",
         },
-        method: options.method,
-        muteHttpExceptions: true,
-        ...(["post", "put"].includes(options.method) && {
-          payload: JSON.stringify(options.body),
-          contentType: "application/json",
-        }),
-      });
-
-      const responseCode = response.getResponseCode();
-      const responseBody = response.getContentText();
-
-      return match(responseCode)
-        .with(200, 201, () => {
-          if (!options.schema) return ok(JSON.parse(responseBody));
-          const parseResult = options.schema.safeParse(JSON.parse(responseBody));
-          return parseResult.success ? ok(parseResult.data) : err(parseResult.error.message);
-        })
-        .with(500, () => {
-          return err("Error: Internal Server Error");
-        })
-        .otherwise(() => err(responseBody));
+        wrappedFetch
+      );
     };
-    return withRetry(
-      {
-        times: 3,
-        delay: 1000,
-        backoff: (count) => count * 1000,
-        shouldRetry: (error) => error === "Error: Internal Server Error",
-      },
-      wrappedFetch
-    );
-  };
+  }
+
+  public setTimeClocks(employId: number, body: EmployeesTimeClocksController_create_body) {
+    const requestUrl = `https://api.freee.co.jp/hr/api/v1/employees/${employId}/time_clocks`;
+    return this.fetch(requestUrl, {
+      method: "post",
+      body,
+      schema: schemas.EmployeesTimeClocksController_create_response,
+    });
+  }
+
+  public getWorkRecord(employId: number, date: string, company_id: number) {
+    const requestUrl = buildUrl(`https://api.freee.co.jp/hr/api/v1/employees/${employId}/work_records/${date}`, {
+      company_id,
+    });
+    return this.fetch(requestUrl, { method: "get", schema: schemas.EmployeesWorkRecordSerializerSchema });
+  }
+
+  public getCompanyEmployees(props: {
+    company_id: number;
+    limit?: number; // 1~100, default:50
+    offset?: number; // pagination
+  }) {
+    // emailを取得したいので"/api/v1/employees"ではなくこちらを使っている
+    // TODO: このエンドポイントはページネーションが不可能なため、100人を超える場合は↑のエントポイントと組み合わせる必要がある？
+    const requestUrl = buildUrl(`https://api.freee.co.jp/hr/api/v1/companies/${props.company_id}/employees`, props);
+    return this.fetch(requestUrl, { method: "get", schema: schemas.CompaniesEmployeeSerializerSchema.array() });
+  }
+
+  public updateWorkRecord(employId: number, date: string, body: EmployeesWorkRecordsController_update_body) {
+    const requestUrl = `https://api.freee.co.jp/hr/api/v1/employees/${employId}/work_records/${date}`;
+    return this.fetch(requestUrl, { method: "put", body, schema: schemas.EmployeesWorkRecordSerializerSchema });
+  }
 }
 
 function withRetry<TReturn>(
@@ -86,36 +122,4 @@ function withRetry<TReturn>(
     if (backoff) Utilities.sleep(backoff(i));
   }
   return err("Error: Unreachable code path");
-}
-
-export function setTimeClocks(employId: number, body: EmployeesTimeClocksController_create_body) {
-  const requestUrl = `https://api.freee.co.jp/hr/api/v1/employees/${employId}/time_clocks`;
-  return fetch(requestUrl, {
-    method: "post",
-    body,
-    schema: schemas.EmployeesTimeClocksController_create_response,
-  });
-}
-
-export function getWorkRecord(employId: number, date: string, company_id: number) {
-  const requestUrl = buildUrl(`https://api.freee.co.jp/hr/api/v1/employees/${employId}/work_records/${date}`, {
-    company_id,
-  });
-  return fetch(requestUrl, { method: "get", schema: schemas.EmployeesWorkRecordSerializerSchema });
-}
-
-export function getCompanyEmployees(props: {
-  company_id: number;
-  limit?: number; // 1~100, default:50
-  offset?: number; // pagination
-}) {
-  // emailを取得したいので"/api/v1/employees"ではなくこちらを使っている
-  // TODO: このエンドポイントはページネーションが不可能なため、100人を超える場合は↑のエントポイントと組み合わせる必要がある？
-  const requestUrl = buildUrl(`https://api.freee.co.jp/hr/api/v1/companies/${props.company_id}/employees`, props);
-  return fetch(requestUrl, { method: "get", schema: schemas.CompaniesEmployeeSerializerSchema.array() });
-}
-
-export function updateWorkRecord(employId: number, date: string, body: EmployeesWorkRecordsController_update_body) {
-  const requestUrl = `https://api.freee.co.jp/hr/api/v1/employees/${employId}/work_records/${date}`;
-  return fetch(requestUrl, { method: "put", body, schema: schemas.EmployeesWorkRecordSerializerSchema });
 }
