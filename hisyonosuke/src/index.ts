18229338b799be753e9583dd03494e047e60664f
Luliko_hub <snow-child@keio.jp>
2023-05-31 02:04:42 +0000

    feat: index.tsの修正

--- hisyonosuke/src/index.ts
+++ hisyonosuke/src/index.ts
@@ -1,17 +1,14 @@
-// import { doPost, init } from "./app";
+import { doPost, init } from "./app";
 import { notificator } from "./birthday-registrator/notificator";
 import { periodicallyCheckForAttendanceManager } from "./attendance-manager/attendanceManager";
 import { notifyPartTimerShift } from "./part-timer-shift/notify";
 import {
-  doPost,
-  // init,
   onOpen,
   callRegistration,
   callShowEvents,
   callModificationAndDeletion,
   insertRegistrationSheet,
   insertModificationAndDeletionSheet,
-  // onInstall,
 } from "./shift-changer/shift-changer";
 /**
  * @file GASエディタから実行できる関数を定義する
@@ -19,14 +16,10 @@ import {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const global: any;
-// global.doPost = doPost;
-// global.init = init;
-// global.notificator = notificator;
-// global.periodicallyCheckForAttendanceManager = periodicallyCheckForAttendanceManager;
-
 global.doPost = doPost;
-// global.init = init;
-// global.onInstall = onInstall;
+global.init = init;
+global.notificator = notificator;
+global.periodicallyCheckForAttendanceManager = periodicallyCheckForAttendanceManager;
 global.onOpen = onOpen;
 global.callRegistration = callRegistration;
 global.callShowEvents = callShowEvents;
