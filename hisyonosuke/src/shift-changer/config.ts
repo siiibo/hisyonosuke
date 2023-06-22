6df6791ac73daa571093ccb921f8598e2d0dfc94
Luliko_hub <snow-child@keio.jp>
2023-06-22 07:04:45 +0000

    refactor: フォーマッターによる整形

--- hisyonosuke/src/shift-changer/config.ts
+++ hisyonosuke/src/shift-changer/config.ts
@@ -1,20 +1,21 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const ConfigSchema = z.object({
   API_URL: z.string(),
-  CALENDAR_ID: z.string(), 
+  CALENDAR_ID: z.string(),
   SLACK_ACCESS_TOKEN: z.string(),
+  SLACK_CHANNEL_TO_POST: z.string(),
   JOB_SHEET_URL: z.string(),
 });
 
-export type Config = z.infer<typeof ConfigSchema>
+export type Config = z.infer<typeof ConfigSchema>;
 
 let config: Config;
 
 export const getConfig = () => {
   if (!config) {
     const props = PropertiesService.getScriptProperties().getProperties();
-    config = ConfigSchema.parse(props)
+    config = ConfigSchema.parse(props);
   }
   return config;
-}
+};
