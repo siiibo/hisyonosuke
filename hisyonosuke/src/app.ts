c0a08cc02e5ff463c28ab2caccffd628c0e7ef44
hi-se <40375981+hi-se@users.noreply.github.com>
2023-11-06 17:05:11 +0900

    refactor: remove celebration-notifier (#59)
    誕生日・記念日の通知Botを https://github.com/siiibo/celebration-notifier
    に移行したため、関連コードを削除した。

--- hisyonosuke/src/app.ts
+++ hisyonosuke/src/app.ts
@@ -1,9 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck //FIXME: strict modeの影響を避けている。次本ファイルを修正する際にこのコメントを解消する
-import { ChannelCreatedEvent, SlackAction, SlackEvent, SlackShortcut, SlackViewAction } from "@slack/bolt";
+import { ChannelCreatedEvent, SlackEvent } from "@slack/bolt";
 import { GasWebClient as SlackClient } from "@hi-se/web-api";
-import { birthdayRegistrator } from "./birthday-registrator/birthday-registrator";
-import { workflowCustomStep } from "./workflow-customstep/workflow-customstep";
 import { initAttendanceManager } from "./attendance-manager/attendanceManager";
 import { shiftChanger } from "./shift-changer/shift-changer-api";
 
@@ -54,9 +52,6 @@ export const doPost = (e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Cont
     return ContentService.createTextOutput(response).setMimeType(ContentService.MimeType.JSON);
   }
 
-  const response = birthdayRegistrator(e); // FIXME: レスポンスの書き換えが生じないようにとりあえずconstで定義してある
-  workflowCustomStep(e);
-
   return ContentService.createTextOutput(response).setMimeType(ContentService.MimeType.JSON);
 };
 
@@ -78,92 +73,11 @@ const isUrlVerification = (e: GoogleAppsScript.Events.DoPost): boolean => {
   }
 };
 
-const isAction = (e: GoogleAppsScript.Events.DoPost): boolean => {
-  if ("payload" in e.parameter) {
-    const type = JSON.parse(e.parameter["payload"])["type"];
-    return type === "block_actions" || type === "workflow_step_edit";
-  }
-  return false;
-};
-
-const isViewAction = (e: GoogleAppsScript.Events.DoPost): boolean => {
-  if (e.parameter.payload) {
-    const type = JSON.parse(e.parameter["payload"])["type"];
-    return type === "view_submission" || type === "view_closed";
-  }
-  return false;
-};
-
-const isShortcut = (e: GoogleAppsScript.Events.DoPost): boolean => {
-  if ("payload" in e.parameter) {
-    const type = JSON.parse(e.parameter["payload"])["type"];
-    return type === "shortcut";
-  }
-  return false;
-};
-
 const isEvent = (e: GoogleAppsScript.Events.DoPost): boolean => {
   if (isJson(e) && e.postData.contents) {
     return "event" in JSON.parse(e.postData.contents);
   }
   return false;
-};
-
-export const getTypeAndCallbackId = (e: GoogleAppsScript.Events.DoPost): { type: string; callback_id: string } => {
-  // FIXME: この関数は使わない方向に修正していく
-  // 詳細は https://github.com/siiibo/hisyonosuke/pull/1 参照
-  if (isAction(e)) {
-    const payload = JSON.parse(e.parameter["payload"]) as SlackAction;
-    switch (payload.type) {
-      case "block_actions":
-        return { type: payload.type, callback_id: payload.view.callback_id };
-      case "workflow_step_edit":
-        return { type: payload.type, callback_id: payload.callback_id };
-      default:
-        return { type: payload.type, callback_id: undefined };
-    }
-  } else if (isViewAction(e)) {
-    const payload = JSON.parse(e.parameter["payload"]) as SlackViewAction;
-    switch (payload.type) {
-      case "view_submission":
-        return { type: payload.type, callback_id: payload.view.callback_id };
-      case "view_closed":
-        return { type: payload.type, callback_id: payload.view.callback_id };
-    }
-  } else if (isShortcut(e)) {
-    const payload = JSON.parse(e.parameter["payload"]) as SlackShortcut;
-    switch (payload.type) {
-      case "shortcut":
-        return { type: payload.type, callback_id: payload.callback_id };
-      case "message_action":
-        return { type: payload.type, callback_id: payload.callback_id };
-    }
-  } else if (isEvent(e)) {
-    const payload = JSON.parse(e.postData.contents).event as SlackEvent;
-    switch (payload.type) {
-      case "workflow_step_execute":
-        return { type: payload.type, callback_id: payload.callback_id };
-      default:
-        return { type: payload.type, callback_id: undefined };
-    }
-  }
-};
-
-// Shujinosukeから移行 // TODO: いつか全体を整えたらコメント消す
-const isOriginalCommand = (target: string, commandRegExpString: string) => {
-  const regExpString = {
-    slackMarkUp: "([*_~`>]|`{3,})*",
-    slackMention: "<@\\w+[\\w\\s|]*>\\s+",
-    commandEnd: "($|[\\s.]+)", // SlackBotのリマインダーで英字コマンドを呼び出すと文末にピリオド(.)が追加される
-  };
-  const commandRegExp = new RegExp(
-    regExpString.slackMention +
-      regExpString.slackMarkUp +
-      commandRegExpString +
-      regExpString.slackMarkUp +
-      regExpString.commandEnd
-  );
-  return target.match(commandRegExp);
 };
 
 const handleChannelCreated = (client: SlackClient, event: ChannelCreatedEvent) => {
