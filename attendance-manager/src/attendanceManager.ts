9dc7880d83cde9357da48e24c274e206d00eff37
たくや <takuyadayo014@gmail.com>
2024-03-12 06:29:36 +0000

    refactor: half_paid_holiday_minsが0の場合含めないように変更した

--- attendance-manager/src/attendanceManager.ts
+++ attendance-manager/src/attendanceManager.ts
@@ -153,7 +153,9 @@ function autoCheckAndClockOut(client: SlackClient, channelId: string, botUserId:
                   clock_in_at: formatDate(workRecord.clock_in_at, "datetime"),
                   clock_out_at: formatDate(workRecord.clock_out_at, "datetime"),
                   note: workRecord.note ? `${workRecord.note} リモート` : "リモート",
-                  half_paid_holiday_mins: workRecord.half_paid_holiday_mins,
+                  ...(workRecord.half_paid_holiday_mins && {
+                    half_paid_holiday_mins: workRecord.half_paid_holiday_mins,
+                  }),
                   break_records: workRecord.break_records.map((record) => {
                     return {
                       clock_in_at: formatDate(record.clock_in_at, "datetime"),
