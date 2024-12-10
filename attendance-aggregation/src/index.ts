bf0219a09c69820536498bfedada42f2d2ab05b7
hi-se <40375981+hi-se@users.noreply.github.com>
2024-12-10 15:46:28 +0900

    refactor(attendance-aggregation): add ignore comment

--- attendance-aggregation/src/index.ts
+++ attendance-aggregation/src/index.ts
@@ -4,6 +4,7 @@ import { init, main, onUserConfigSheetOpen } from "./main";
  * @file GASエディタから実行できる関数を定義する
  */
 
+// biome-ignore lint/suspicious/noExplicitAny: lint/suspicious/noExplicitAny:
 declare const global: any;
 global.main = main;
 global.init = init;
