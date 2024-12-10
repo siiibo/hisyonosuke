102b0245df6d7aecf6195f48bb1ccbb18b1bb375
hi-se <40375981+hi-se@users.noreply.github.com>
2024-12-10 16:39:16 +0900

    refactor(attendance-manager):　lint/style/useNodejsImportProtocolの修正

--- attendance-manager/src/userWorkStatus.test.ts
+++ attendance-manager/src/userWorkStatus.test.ts
@@ -1,4 +1,4 @@
-import assert from "assert";
+import assert from "node:assert";
 import type { CommandType } from "./command";
 import { WORK_STATUS, getUserWorkStatusByCommands } from "./userWorkStatus";
 
